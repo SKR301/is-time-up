27bbfae46443db70c434dbdda7e29dd4250e5c64
Saurav KumaR <chunnu.bihat@gmail.com>
2021-09-28 16:36:09 +0530

    basic gameplay done and scoring-1 #2 #10

--- js/game.js
+++ js/game.js
@@ -2,16 +2,22 @@ var timeBegin = 0;
 var timeEnd = 0;
 var actualTime = 0;
 var yourTime = 0;
+var totalScore = 0;
 
 document.getElementById("startTimer_btn").disabled  = true;
 document.getElementById("result").style.visibility  = 'hidden';
 
-
 function genTime(){
-	document.getElementById("getTime").remove();
-	document.getElementById('time').style.visibility  = 'visible';
+	var time = document.getElementById("getTime");
+	time.classList.remove("btn");
+	time.classList.remove("btn-primary");
+	time.classList.remove("rounded");
+	time.setAttribute('onclick','');
+	time.classList.add("text-primary");
+
+
 	actualTime = Math.floor(Math.random() * 9 + 1);
-	document.getElementById("time").innerHTML = actualTime;
+	time.innerHTML = actualTime;
 	document.getElementById("startTimer_btn").disabled = false;
 }
 
@@ -22,7 +28,7 @@ function startTime(){
 	btn.classList.remove("btn-success");
 	btn.classList.add("btn-danger");
 	btn.innerHTML = "STOP";
-	btn.setAttribute('onclick','stopTime()')
+	btn.setAttribute('onclick','stopTime()');
 	btn.id = "stopTimer_btn";
 }
 
@@ -30,6 +36,16 @@ function stopTime(){
 	timeEnd = new Date();
 	yourTime = (timeEnd - timeBegin) / 1000;
 	showResult();
+
+	var btn = document.getElementById("stopTimer_btn");
+	btn.classList.remove("btn-danger");
+	btn.classList.add("btn-success");
+	btn.innerHTML = "START";
+	btn.setAttribute('onclick','startTime()');
+	btn.id = "startTimer_btn";
+	btn.disabled = true;
+
+	calcScore();
 }
 
 function showResult(){
@@ -43,14 +59,20 @@ function closeResult(){
 	document.getElementById("result").style.visibility  = 'hidden';
 	document.getElementById("game").style.visibility  = 'visible';
 
-	var btn = document.getElementById("stopTimer_btn");
-	btn.classList.remove("btn-danger");
-	btn.classList.add("btn-success");
-	btn.innerHTML = "START";
-	btn.setAttribute('onclick','startTime()')
-	btn.id = "startTimer_btn";
+	var time = document.getElementById("getTime");
+	time.classList.add("btn");
+	time.classList.add("btn-primary");
+	time.classList.add("rounded");
+	time.setAttribute('onclick','genTime()');
+	time.classList.remove("text-primary");
+	time.innerHTML = "Get Time";
+}
 
-
+//Need to change the scoring pattern;
+function calcScore(){
+	score = 10 - Math.abs(actualTime - yourTime);
+	totalScore += score;
+	document.getElementById('score').innerHTML = "SCORE : " + totalScore;
 }
 
 function saveScore(){
